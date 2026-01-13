8b50e8ea0c376df318fd5e7da711b9f86e62a3eb
Azemed <azemedcs2@proton.me>
2026-01-13 21:28:37 +0500

    Optimized the code and also made shuffling function

--- main.cpp
+++ main.cpp
@@ -4,64 +4,82 @@
 using namespace std;
 
 struct Bars{
-	float height;
-	Color color;
+    float height;
+    Color color;
 };
 
+int i = 0;
+int j = 0;
+
+void ShuffleArray(Bars bars[], int length){
+    for(int k = 0; k < length; k++){
+        bars[k].height = GetRandomValue(10, 750);
+        bars[k].color = RED;
+    }
+    i = 0;
+    j = 0;
+}
+
 void BubbleSort(Bars array[], int length) {
-
-	//used static variables so it can remember em
-    static int i = 0;
-    static int j = 0;
-
-    if (i >= length - 1) return;
+    if (i >= length - 1){
+		WaitTime(10);
+        ShuffleArray(array, length);
+        return;
+    }
 
     if (array[j].height > array[j + 1].height) {
         Bars temp = array[j];
         array[j] = array[j+1];
         array[j+1] = temp;
+        
     }
 
     j++;
     if (j >= length - i - 1) {
+        array[j].color = GREEN; 
+        
         j = 0;
         i++;
     }
 }
+
 int main()
 {
-	float screen_width = 1600;
-	float screen_height = 900;
+    const float screen_width = 1600;
+    const float screen_height = 900;
 
     InitWindow(screen_width, screen_height, "Algorithm Visualizer");
-	int size = 400;
-	Bars bars[size];
-	SetTargetFPS(180);
+    
+    const int size = 100; 
+    Bars bars[size];
+    
+    SetTargetFPS(180); 
 
-	for(auto &bar : bars){
-		bar.height = GetRandomValue(10,750);
-		bar.color = RED;
-	}
-
-	for (auto bar : bars){
-		cout << bar.height << endl;
-	}
-
+    ShuffleArray(bars, size);
 
     while (!WindowShouldClose())
     {
-		BubbleSort(bars, size);
+        BubbleSort(bars, size);
+        
         BeginDrawing();
             ClearBackground(BLACK);
-			float i = 0;
-			for(const auto &bar : bars)
-			{
+            
+            float x = 0;
+            // Calculate width dynamically
+            float barWidth = screen_width / size; 
 
-				Rectangle rec = {i, screen_height - bar.height, screen_width/size, bar.height};
-				DrawRectangleRec(rec, RED);
-				DrawRectangleLinesEx(rec, 3 , RAYWHITE);
-				i += screen_width/size;
-			}
+            for(const auto &bar : bars)
+            {
+                Rectangle rec = {x, screen_height - bar.height, barWidth, bar.height};
+                
+                DrawRectangleRec(rec, bar.color);
+                
+                if (barWidth > 5) {
+                    DrawRectangleLinesEx(rec, 1, RAYWHITE);
+                }
+                
+                x += barWidth;
+            }
 
         EndDrawing();
     }
